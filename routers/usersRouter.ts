e7f31b16fb2ffc8b4bcc7e19b3f1297b64ae6915
vladcheck <valdemarcheck@yandex.ru>
2026-03-04 10:46:37 +0300

    feat: document usersRouter

--- routers/usersRouter.ts
+++ routers/usersRouter.ts
@@ -7,10 +7,80 @@ import { Router } from "express";
 
 const usersRouter: Router = Router();
 
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *    User:
+ *      type: object
+ *      required:
+ *       - id
+ *       - first_name
+ *       - last_name
+ *       - email
+ *       - password
+ *      properties:
+ *        id:
+ *          type: string
+ *          description: Уникальный ID пользователя
+ *        first_name:
+ *          type: string
+ *          description: Имя
+ *        last_name:
+ *          type: string
+ *          description: Фамилия
+ *        email:
+ *          type: string
+ *          description: Почта, она же и логин
+ *        password:
+ *          type: string
+ *          description: Пароль
+ *      example:
+ *        id: "adfs31"
+ *        first_name: Сергей
+ *        last_name: Овчинников
+ *        email: sergei_ovchinnikov@yandex.ru
+ *        password: "12345678"
+ */
+
+/**
+ * @swagger
+ * /api/users:
+ *  get:
+ *    summary: Получить всех пользователей
+ *    tags: [Users]
+ *    responses:
+ *      200:
+ *        descrition: Пользователи найдены
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                $ref: '#/components/schemas/User'
+ */
 usersRouter.get("/", async (_req: Request, res: Response) => {
   return res.status(StatusCodes.OK).json(users);
 });
 
+/**
+ * @swagger
+ * /api/users/:id:
+ *  get:
+ *    summary: Получить пользователя по ID
+ *    tags: [Users]
+ *    responses:
+ *      400:
+ *        description: ID не определен
+ *      404:
+ *        description: Пользователя с таким ID не существует
+ *      200:
+ *        description: Пользователь найден
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/User'
+ */
 usersRouter.get("/:id", async (req: Request, res: Response) => {
   const { id } = req.params;
   if (!id) {
