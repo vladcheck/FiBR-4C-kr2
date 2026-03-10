05ba2f8f1996ec95c53d3b2d820f2695ed42f15d
vladcheck <valdemarcheck@yandex.ru>
2026-03-10 13:40:53 +0300

    feat: implement grantRefreshToken method

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 const JWT_CONFIG = {
   ACCESS_SECRET: "access_secret",
   REFRESH_SECRET: "refresh_secret",
-  EXP: 900, // 15m
+  EXP: "15m",
   REFRESH_EXP: "28d",
 };
 
@@ -21,6 +21,23 @@ class JwtSingleton {
       JWT_CONFIG.ACCESS_SECRET,
       {
         expiresIn: expiresIn,
+      },
+    );
+  }
+
+  grantRefreshToken(
+    sub: string,
+    body: [string, any][],
+    expiresIn: any = JWT_CONFIG.REFRESH_EXP,
+  ): string {
+    return jwt.sign(
+      {
+        sub,
+        ...body,
+      },
+      JWT_CONFIG.REFRESH_SECRET,
+      {
+        expiresIn,
       },
     );
   }
