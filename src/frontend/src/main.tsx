0a5698d2f29a4d0544fe087e8d9fcf59f5c087f6
vladcheck <valdemarcheck@yandex.ru>
2026-03-12 11:28:30 +0300

    fix: wrong name

--- src/frontend/src/main.tsx
+++ src/frontend/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router";
 import ApiWrapper from "./features/api/ApiWrapper.tsx";
-import SiteRoutes from "./Routes.tsx";
+import SiteRoutes from "./routes.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
