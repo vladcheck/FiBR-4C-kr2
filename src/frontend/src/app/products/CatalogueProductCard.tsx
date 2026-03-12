f280702c3e88eebd54cd5ca31daf84ccc50f076b
vladcheck <valdemarcheck@yandex.ru>
2026-03-12 11:27:54 +0300

    feat: setup mock catalogue page

---
+++ src/frontend/src/app/products/CatalogueProductCard.tsx
@@ -0,0 +1,34 @@
+import { Link } from "react-router";
+import Product from "../../entities/Product.ts";
+import FlexContainer from "../../shared/ui/FlexContainer.tsx";
+
+export default function CatalogueProductCard({ p }: { p: Product }) {
+  return (
+    <FlexContainer
+      flexDir="col"
+      justify="center"
+      align="center"
+      className="h-full w-full border-3 border-red-900"
+    >
+      <FlexContainer flexDir="col" justify="center" className="w-full">
+        <Link to={`/products/${p.id}`}>
+          {p.images?.[0] ? (
+            <img className="min-w-40 w-max aspect-4/5" src="" alt="" />
+          ) : (
+            <div className="min-w-40 w-max aspect-4/5 bg-gray-200"></div>
+          )}
+        </Link>
+        <span className="block">{p.category}</span>
+        <span className="block text-gray-800">{p.price}</span>
+        <Link to={`/products/${p.id}`}>
+          <h2 className="text-[1.2rem]">{p.title}</h2>
+        </Link>
+        {p.description && (
+          <p className="break-after-all wrap-break-word text-gray-600">
+            {p.description}
+          </p>
+        )}
+      </FlexContainer>
+    </FlexContainer>
+  );
+}
---
+++ src/frontend/src/app/products/page.tsx
@@ -0,0 +1,40 @@
+import { useContext, useEffect, useState } from "react";
+import Product from "../../entities/Product";
+import CatalogueProductCard from "./CatalogueProductCard";
+import ApiContext from "../../features/api/ApiContext";
+import FlexContainer from "../../shared/ui/FlexContainer";
+
+export default function ProductsPage() {
+  const { api } = useContext(ApiContext);
+  const [, setProducts] = useState<Product[]>([]);
+  const [filteredProducts, setFilteredProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    api
+      ?.getProducts()
+      .then((res) => {
+        setProducts(res.data);
+        setFilteredProducts(res.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [api]);
+
+  return (
+    <FlexContainer className="gap-4 pt-6">
+      <aside className="p-2 flex h-min flex-col min-w-50 border border-gray-200"></aside>
+      {filteredProducts ? (
+        <div className="flex-1 grid grid-cols-5 p-2 gap-2 justify-items-center justify-center">
+          {filteredProducts.map((p) => (
+            <CatalogueProductCard key={p.id} p={p} />
+          ))}
+        </div>
+      ) : (
+        <FlexContainer justify="center" align="center" className="gap-2">
+          <h2 className="text-2xl">Мы не нашли товаров по вашему запросу</h2>
+        </FlexContainer>
+      )}
+    </FlexContainer>
+  );
+}
