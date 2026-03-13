ecb4b1c83132b803a4bb9a27cf55b8216d72ce55
vladcheck <valdemarcheck@yandex.ru>
2026-03-13 17:40:17 +0300

    feat: add handler to DELETE users

--- src/server/routers/usersRouter.ts
+++ src/server/routers/usersRouter.ts
@@ -1,8 +1,11 @@
 import type { Request, Response } from "express";
-import { getBadRequest, getNotFound } from "../utils/requestHelpers";
+import { getBadRequest, getNotFound, getOk } from "../utils/requestHelpers";
 import { StatusCodes } from "http-status-codes";
-import users from "../mock/users";
+import initialUsers from "../mock/users";
 import { Router } from "express";
+import authMiddleware from "../middleware/authMiddleware";
+
+let users = initialUsers;
 
 const usersRouter: Router = Router();
 
@@ -79,18 +82,50 @@ usersRouter.get("/", async (_req: Request, res: Response) => {
  *          application/json:
  *            schema:
  *              $ref: '#/components/schemas/User'
+ *  delete:
+ *    summary: Удалить пользователя по ID
+ *    tags: [Users]
+ *    responses:
+ *      400:
+ *        description: ID не определен
+ *      404:
+ *        description: Пользователя с таким ID не существует
+ *      200:
+ *        description: Пользователь удален
  */
-usersRouter.get("/:id", async (req: Request, res: Response) => {
-  const { id } = req.params;
-  if (!id) {
-    return getBadRequest(res);
-  }
-  const user = users.find((u) => u.id === id);
-  if (!user) {
-    return getNotFound(res);
-  }
+usersRouter
+  .get("/:id", async (req: Request, res: Response) => {
+    const { id } = req.params;
+    if (!id) {
+      return getBadRequest(res);
+    }
+    const user = users.find((u) => u.id === id);
+    if (!user) {
+      return getNotFound(res);
+    }
 
-  return res.status(StatusCodes.OK).json(user);
-});
+    return res.status(StatusCodes.OK).json(user);
+  })
+  .delete("/:id", authMiddleware, async (req: Request, res: Response) => {
+    const { id } = req.params;
+    if (!id) {
+      return getBadRequest(res, "uid not provided");
+    }
+
+    let index = -1;
+    const user = users.find((u, i) => {
+      if (u.id === id) {
+        index = i;
+        return true;
+      }
+      return false;
+    });
+    if (!user && index === -1) {
+      return getNotFound(res, `user with id ${id} was not found`);
+    }
+
+    users = users.toSpliced(index, 1);
+    return getOk(res, "user deleted");
+  });
 
 export default usersRouter;
