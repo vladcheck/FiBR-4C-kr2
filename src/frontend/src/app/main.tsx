01b2e599b303db71052f817773ea1ed873480dbd
vladcheck <valdemarcheck@yandex.ru>
2026-03-16 18:04:35 +0300

    fix: put pages into pages/ layer, core files in app/ layer

--- src/frontend/src/app/main.tsx
+++ src/frontend/src/app/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router";
-import ApiWrapper from "./features/api/ApiWrapper.tsx";
+import ApiWrapper from "../features/api/ApiWrapper.tsx";
 import SiteRoutes from "./routes.tsx";
 
 createRoot(document.getElementById("root")!).render(
--- src/frontend/src/app/routes.tsx
+++ src/frontend/src/app/routes.tsx
@@ -1,13 +1,13 @@
 import { Route, Routes } from "react-router";
-import LoginPage from "./app/login/page.tsx";
-import RegisterPage from "./app/register/page.tsx";
-import RootLayout from "./app/layout.tsx";
-import Page from "./app/page.tsx";
-import ProductsPage from "./app/products/page.tsx";
-import ProductPage from "./app/products/[:id]/page.tsx";
-import CreateProductPage from "./app/products/create/page.tsx";
-import NotFoundPage from "./app/notFound.tsx";
-import ProfilePage from "./app/profile/page.tsx";
+import LoginPage from "../pages/login/page.tsx";
+import RegisterPage from "../pages/register/page.tsx";
+import RootLayout from "../pages/layout.tsx";
+import Page from "../pages/page.tsx";
+import ProductsPage from "../pages/products/page.tsx";
+import ProductPage from "../pages/products/[:id]/page.tsx";
+import CreateProductPage from "../pages/products/create/page.tsx";
+import NotFoundPage from "../pages/notFound.tsx";
+import ProfilePage from "../pages/profile/page.tsx";
 
 export default function SiteRoutes() {
   return (
