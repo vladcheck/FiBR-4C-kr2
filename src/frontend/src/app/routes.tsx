128460d51fe172b8d29460eb6fb726009dec6566
vladcheck <valdemarcheck@yandex.ru>
2026-03-16 18:05:17 +0300

    feat: make /shop an index page

--- src/frontend/src/app/routes.tsx
+++ src/frontend/src/app/routes.tsx
@@ -2,7 +2,6 @@ import { Route, Routes } from "react-router";
 import LoginPage from "../pages/login/page.tsx";
 import RegisterPage from "../pages/register/page.tsx";
 import RootLayout from "../pages/layout.tsx";
-import Page from "../pages/page.tsx";
 import ProductsPage from "../pages/products/page.tsx";
 import ProductPage from "../pages/products/[:id]/page.tsx";
 import CreateProductPage from "../pages/products/create/page.tsx";
@@ -13,7 +12,7 @@ export default function SiteRoutes() {
   return (
     <Routes>
       <Route element={<RootLayout />}>
-        <Route path="/" element={<Page />} />
+        <Route index element={<ProductsPage />} />
         <Route path="/shop" element={<ProductsPage />} />
         <Route path="/products/:id" element={<ProductPage />} />
         <Route path="/products/create" element={<CreateProductPage />} />
